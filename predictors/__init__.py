0df43a5350cf76105e80537a2e733d3f2d337b93
BigMacNinja <codyellis41893@gmail.com>
2025-08-26 18:38:47 -0400

    feat(predictors): add predictors package init

---
+++ predictors/__init__.py
@@ -0,0 +1,8 @@
+"""Predictors package for Iris models.
+
+Usage:
+    from predictors.knn import predict as knn_predict
+    from predictors.forest import predict as forest_predict
+"""
+
+__all__ = ["knn", "forest"]
